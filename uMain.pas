db7f220eca29a0d9c7a95a67c600d35a15265dd9
AOlifer <AOlifer@yahoo.com>
2022-12-02 22:46:34 +0300

    Генерация с условиями:
      -- первый символ из Верхнего или Нижнего регистра
      -- обязательно иметь хотя бы один символ из каждого набора

--- uMain.pas
+++ uMain.pas
@@ -41,23 +41,41 @@ implementation
 {$R *.fmx}
 
 procedure TfMain.btnGenerateClick(Sender: TObject);
+const
+  emp = Char(nil);
 var
   i :integer;
-  str :string;
+  res :string;
+  procedure add_random_char(str :string);
+  var n :integer;
+  begin
+    Repeat
+      n := Random(psw_lenght - 1) + 2;
+    Until (p[n] = emp);
+    p[n] := str[Random(Length(str)) + 1];
+  end;
 begin
   //очистка
-  for i := 1 to psw_lenght do p[i] := Char(nil);
-
+  for i := 1 to max_lenght do p[i] := Char(nil);
+  //Добавляем первый символ из Верхнего или Нижнего регистра
+  p[1] := (str_UPPER+str_LOWER)[Random(Length(str_UPPER+str_LOWER)) + 1];
+  //Добавляем символ из Верхнего регистра
+  add_random_char(str_UPPER);
+  //Добавляем символ из Нижнего регистра
+  add_random_char(str_LOWER);
+  //Добавляем символ из Набора цифр
+  add_random_char(str_NUMBS);
+  //Добавляем символ из Набора спецсимволов (не может быть в начале пароля)
+  add_random_char(str_CHARS);
   //заполняем массив случайными символами
-  for i := 1 to psw_lenght do begin
-    if p[i] = Char(nil) then
+  for i := 2 to psw_lenght do begin
+    if p[i] = emp then
       p[i] := str_COMBS[Random(psw_chrcnt) + 1];
   end;
-
   //формируем результат
-  str := EmptyStr;
-  for i := 1 to psw_lenght do str := str + p[i];
-  eNewPassword.Text := str;
+  res := EmptyStr;
+  for i := 1 to psw_lenght do res := res + p[i];
+  eNewPassword.Text := res;
 end;
 
 end.
