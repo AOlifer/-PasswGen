f8346aa880d7255a0fafe971fce67f8792e68f6f
AOlifer <aolifer@bescargo.ru>
2022-12-02 16:38:22 +0300

    Create project

---
+++ uMain.pas
@@ -0,0 +1,24 @@
+unit uMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TfMain = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fMain: TfMain;
+
+implementation
+
+{$R *.fmx}
+
+end.
