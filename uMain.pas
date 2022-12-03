ea1c4d4c632dda29d87ac2f44fdb5ea58c15f2b8
AOlifer <AOlifer@yahoo.com>
2022-12-03 11:49:54 +0300

    Генерация с комбинированием условий

--- uMain.pas
+++ uMain.pas
@@ -5,13 +5,40 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Edit,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.EditBox, FMX.SpinBox, FMX.Layouts,
+  FMX.Memo.Types, FMX.ScrollBox, FMX.Memo;
 
 type
   TfMain = class(TForm)
     btnGenerate: TButton;
+    ePswLength: TSpinBox;
+    pPswEdit: TGridPanelLayout;
+    eOldPassword: TEdit;
     eNewPassword: TEdit;
+    pAlphabet: TGroupBox;
+    lAlphabet: TGridPanelLayout;
+    lStandart: TGridPanelLayout;
+    lManual: TGridPanelLayout;
+    rbManual: TRadioButton;
+    rbStandart: TRadioButton;
+    lCheckButtons: TGridPanelLayout;
+    chStrUpper: TCheckBox;
+    chStrLower: TCheckBox;
+    chStrNumbs: TCheckBox;
+    chStrChars: TCheckBox;
+    eStrUpper: TEdit;
+    eStrLower: TEdit;
+    eStrNumbs: TEdit;
+    eStrChars: TEdit;
+    eManual: TMemo;
+    pBottom: TPanel;
+    lbAlhabetCnt: TLabel;
+    lbPswLength: TLabel;
     procedure btnGenerateClick(Sender: TObject);
+    procedure rbStandartDblClick(Sender: TObject);
+    procedure rbClick(Sender: TObject);
+    procedure chClick(Sender: TObject);
+    function LoadAlphabet: boolean;
   private
     { Private declarations }
   public
@@ -22,18 +49,20 @@ var
   fMain: TfMain;
 
 const
+  minAlhabetCnt = 36;
   str_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   str_LOWER = 'abcdefghijklmnopqrstuvwxyz';
   str_NUMBS = '0123456789';
   str_CHARS = '@#$&*%_';
   str_COMBS = str_UPPER + str_LOWER + str_NUMBS + str_CHARS;
-  psw_chrcnt = Length(str_COMBS);
-  psw_lenght = 10;
   min_lenght = 6;
   max_lenght = 50;
 
+
 var
   p :array [1 .. max_lenght] of Char;
+  v_UPPER,v_LOWER,v_NUMBS,v_CHARS,v_COMBS :string;
+  AlhabetCnt,psw_lenght :integer;
 
 implementation
 
@@ -44,7 +73,7 @@ procedure TfMain.btnGenerateClick(Sender: TObject);
 const
   emp = Char(nil);
 var
-  i :integer;
+  i,inc :integer;
   res :string;
   procedure add_random_char(str :string);
   var n :integer;
@@ -55,27 +84,96 @@ var
     p[n] := str[Random(Length(str)) + 1];
   end;
 begin
-  //очистка
-  for i := 1 to max_lenght do p[i] := Char(nil);
-  //Добавляем первый символ из Верхнего или Нижнего регистра
-  p[1] := (str_UPPER+str_LOWER)[Random(Length(str_UPPER+str_LOWER)) + 1];
-  //Добавляем символ из Верхнего регистра
-  add_random_char(str_UPPER);
-  //Добавляем символ из Нижнего регистра
-  add_random_char(str_LOWER);
-  //Добавляем символ из Набора цифр
-  add_random_char(str_NUMBS);
-  //Добавляем символ из Набора спецсимволов (не может быть в начале пароля)
-  add_random_char(str_CHARS);
-  //заполняем массив случайными символами
-  for i := 2 to psw_lenght do begin
-    if p[i] = emp then
-      p[i] := str_COMBS[Random(psw_chrcnt) + 1];
+  if LoadAlphabet then begin
+    inc := 0;
+    //очистка
+    for i := 1 to max_lenght do p[i] := emp;
+    //Добавляем первый символ из Верхнего или Нижнего регистра
+    p[1] := (str_UPPER+str_LOWER)[Random(Length(str_UPPER+str_LOWER)) + 1];
+    inc := inc + 1;
+    //Добавляем символ из Верхнего регистра
+    if chStrUpper.IsChecked then add_random_char(v_UPPER);
+    //Добавляем символ из Нижнего регистра
+    if chStrLower.IsChecked then add_random_char(v_LOWER);
+    //Добавляем символ из Набора цифр
+    if chStrNumbs.IsChecked then add_random_char(v_NUMBS);
+    //Добавляем символ из Набора спецсимволов (не может быть в начале пароля)
+    if chStrChars.IsChecked then add_random_char(str_CHARS);
+    //заполняем массив случайными символами
+    for i := (1 + inc) to psw_lenght do begin
+      if p[i] = emp then
+        p[i] := v_COMBS[Random(AlhabetCnt) + 1];
+    end;
+    //формируем результат
+    res := EmptyStr;
+    for i := 1 to psw_lenght do res := res + p[i];
+    eNewPassword.Text := res;
   end;
-  //формируем результат
-  res := EmptyStr;
-  for i := 1 to psw_lenght do res := res + p[i];
-  eNewPassword.Text := res;
+end;
+
+function TfMain.LoadAlphabet: boolean;
+  procedure ResetStr;
+  begin
+    v_UPPER := EmptyStr;
+    v_LOWER := EmptyStr;
+    v_NUMBS := EmptyStr;
+    v_CHARS := EmptyStr;
+    v_COMBS := EmptyStr;
+  end;
+  function DistinctChar(str :string): string;
+  begin
+    Result := str;
+  end;
+begin
+  Result := False;
+  if rbManual.IsChecked then begin
+    if eManual.Text = EmptyStr then Exit;
+    ResetStr;
+    v_COMBS := DistinctChar(eManual.Text);
+  end else begin
+    if (chStrUpper.IsChecked and (eStrUpper.Text <> EmptyStr)) or
+       (chStrLower.IsChecked and (eStrLower.Text <> EmptyStr)) or
+       (chStrNumbs.IsChecked and (eStrNumbs.Text <> EmptyStr)) or
+       (chStrChars.IsChecked and (eStrChars.Text <> EmptyStr)) then begin
+      ResetStr;
+      if chStrUpper.IsChecked then
+        v_UPPER := DistinctChar(eStrUpper.Text);
+      if chStrLower.IsChecked then
+        v_LOWER := DistinctChar(eStrLower.Text);
+      if chStrNumbs.IsChecked then
+        v_NUMBS := DistinctChar(eStrNumbs.Text);
+      if chStrChars.IsChecked then
+        v_CHARS := DistinctChar(eStrChars.Text);
+      v_COMBS := DistinctChar(v_UPPER + v_LOWER + v_NUMBS + v_CHARS);
+    end else Exit;
+  end;
+  psw_lenght := StrToInt(ePswLength.Text);
+  AlhabetCnt := Length(V_COMBS);
+  eManual.Text := V_COMBS;
+  lbAlhabetCnt.Text := 'Мощность алфавита: ' + IntToStr(AlhabetCnt) + ' симв.';
+  Result := True;
+end;
+
+procedure TfMain.rbStandartDblClick(Sender: TObject);
+begin
+  eStrUpper.Text := str_upper;
+  eStrLower.Text := str_lower;
+  eStrNumbs.Text := str_numbs;
+  eStrChars.Text := str_chars;
+end;
+
+procedure TfMain.rbClick(Sender: TObject);
+begin
+  lCheckButtons.Enabled := rbStandart.IsChecked;
+  eManual.Enabled := rbManual.IsChecked;
+end;
+
+procedure TfMain.chClick(Sender: TObject);
+begin
+  eStrUpper.Enabled := chStrUpper.IsChecked;
+  eStrLower.Enabled := chStrLower.IsChecked;
+  eStrNumbs.Enabled := chStrNumbs.IsChecked;
+  eStrChars.Enabled := chStrChars.IsChecked;
 end;
 
 end.
